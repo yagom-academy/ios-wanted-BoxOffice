5aa7d24eeeed28ca468a4005367dd6c7e30087d0
강민교 <98341623+KangMinGyo@users.noreply.github.com>
2022-10-17 22:37:53 +0900

    프로젝트 초기 세팅

---
+++ BoxOffice/Views/BoxOffice/BoxOfficeView.swift
@@ -0,0 +1,8 @@
+//
+//  BoxOfficeView.swift
+//  BoxOffice
+//
+//  Created by KangMingyo on 2022/10/17.
+//
+
+import UIKit
--- BoxOffice/Views/BoxOffice/BoxOfficeViewController.swift
+++ BoxOffice/Views/BoxOffice/BoxOfficeViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class BoxOfficeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
