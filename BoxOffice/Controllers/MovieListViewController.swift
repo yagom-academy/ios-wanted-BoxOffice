ce7cd570c21b4ab9cf18de0c9bf0347ccff5236f
Subin Kim <skyqnaqna@naver.com>
2022-10-18 03:59:28 +0900

    style: Indent width 수정

--- BoxOffice/Controllers/MovieListViewController.swift
+++ BoxOffice/Controllers/MovieListViewController.swift
@@ -9,12 +9,12 @@ import UIKit
 
 class MovieListViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-        self.navigationItem.title = "Box Office"
-        self.view.backgroundColor = .white
-    }
+    self.navigationItem.title = "Box Office"
+    self.view.backgroundColor = .white
+  }
 
 
 }
@@ -22,28 +22,28 @@ class MovieListViewController: UIViewController {
 #if canImport(SwiftUI) && DEBUG
 import SwiftUI
 extension UIViewController {
-    struct ViewControllerPreview: UIViewControllerRepresentable {
-        let viewController: UIViewController
+  struct ViewControllerPreview: UIViewControllerRepresentable {
+    let viewController: UIViewController
 
-        func makeUIViewController(context: Context) -> UIViewController {
-            return viewController
-        }
-
-        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
-
-        }
+    func makeUIViewController(context: Context) -> UIViewController {
+      return viewController
     }
 
-    func showPreview() -> some View {
-        ViewControllerPreview(viewController: self)
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+
     }
+  }
+
+  func showPreview() -> some View {
+    ViewControllerPreview(viewController: self)
+  }
 }
 #endif
 
 #if canImport(SwiftUI) && DEBUG
 struct MovieListVC_Preview: PreviewProvider {
-    static var previews: some View {
-        MovieListViewController().showPreview()
-    }
+  static var previews: some View {
+    MovieListViewController().showPreview()
+  }
 }
 #endif
--- BoxOffice/Extensions/Extension+Bundle.swift
+++ BoxOffice/Extensions/Extension+Bundle.swift
@@ -8,52 +8,52 @@
 import Foundation
 
 extension Bundle {
-    var omdbKey: String {
-        guard let path = Bundle.main.url(forResource: "SecretInfo", withExtension: "plist") else {
-            return ""
-        }
-
-        do {
-            let data = try Data(contentsOf: path)
-
-            guard let dict = try PropertyListSerialization.propertyList(from: data, format: nil) as? [String: Any] else {
-                return ""
-            }
-
-            guard let key = dict["OMDb_API_KEY"] as? String else {
-                fatalError("SecretInfo.plist에 OMDb API Key를 설정해주세요.")
-            }
-
-            return key
-
-        } catch {
-            print(error)
-            return ""
-        }
+  var omdbKey: String {
+    guard let path = Bundle.main.url(forResource: "SecretInfo", withExtension: "plist") else {
+      return ""
     }
 
-    var kobisKey: String {
-        guard let path = Bundle.main.url(forResource: "SecretInfo", withExtension: "plist") else {
-            return ""
-        }
+    do {
+      let data = try Data(contentsOf: path)
 
-        do {
-            let data = try Data(contentsOf: path)
+      guard let dict = try PropertyListSerialization.propertyList(from: data, format: nil) as? [String: Any] else {
+        return ""
+      }
 
-            guard let dict = try PropertyListSerialization.propertyList(from: data, format: nil) as? [String: Any] else {
-                return ""
-            }
+      guard let key = dict["OMDb_API_KEY"] as? String else {
+        fatalError("SecretInfo.plist에 OMDb API Key를 설정해주세요.")
+      }
 
-            guard let key = dict["Kobis_API_KEY"] as? String else {
-                fatalError("SecretInfo.plist에 Kobis API Key를 설정해주세요.")
-            }
+      return key
 
-            return key
-
-        } catch {
-            print(error)
-            return ""
-        }
+    } catch {
+      print(error)
+      return ""
     }
+  }
+
+  var kobisKey: String {
+    guard let path = Bundle.main.url(forResource: "SecretInfo", withExtension: "plist") else {
+      return ""
+    }
+
+    do {
+      let data = try Data(contentsOf: path)
+
+      guard let dict = try PropertyListSerialization.propertyList(from: data, format: nil) as? [String: Any] else {
+        return ""
+      }
+
+      guard let key = dict["Kobis_API_KEY"] as? String else {
+        fatalError("SecretInfo.plist에 Kobis API Key를 설정해주세요.")
+      }
+
+      return key
+
+    } catch {
+      print(error)
+      return ""
+    }
+  }
 
 }
