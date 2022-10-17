89121bc9e619740a1de7ffa24ffcdf42a88656b8
betterhee <betterhee@gmail.com>
2022-10-17 16:05:07 +0900

    feat: Add MovieListViewController

--- BoxOffice/View Controllers/MovieListViewController.swift
+++ BoxOffice/View Controllers/MovieListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MovieListViewController.swift
 //  BoxOffice
 //
 //  Created by kjs on 2022/10/14.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MovieListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
