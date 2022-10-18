cd0984a41e68ea0621c2bfb043419aec448a51df
강민교 <98341623+KangMinGyo@users.noreply.github.com>
2022-10-18 15:47:32 +0900

    design: BoxOfficeView UI 추가

--- BoxOffice/Views/BoxOffice/BoxOfficeView.swift
+++ BoxOffice/Views/BoxOffice/BoxOfficeView.swift
@@ -6,3 +6,66 @@
 //
 
 import UIKit
+
+class BoxOfficeView: UIView {
+    
+    let boxOfficeTableView: UITableView = {
+         let tableView = UITableView()
+         tableView.rowHeight = 150
+         tableView.translatesAutoresizingMaskIntoConstraints = false
+         return tableView
+     }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addView()
+        configure()
+        setupTableView()
+    }
+    
+    required init?(coder NSCoder : NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupTableView() {
+        boxOfficeTableView.delegate = self
+        boxOfficeTableView.dataSource = self
+        boxOfficeTableView.register(BoxOfficeTableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    func addView() {
+        addSubview(boxOfficeTableView)
+    }
+    
+    func configure() {
+        NSLayoutConstraint.activate([
+            boxOfficeTableView.topAnchor.constraint(equalTo: topAnchor),
+            boxOfficeTableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            boxOfficeTableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            boxOfficeTableView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}
+
+extension BoxOfficeView: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = boxOfficeTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! BoxOfficeTableViewCell
+        cell.posterImageView.image = UIImage(named: "image")
+        cell.rankOldAndNew.text = "New"
+        cell.movieName.text = "영화이름"
+        cell.openDate.text = "2022-10-12"
+        cell.boxOfficeRank.text = "1"
+        cell.rankInten.text = "-1"
+        cell.audiAcc.text = "820만"
+    
+        return cell
+    }
+    
+    
+}
--- BoxOffice/Views/BoxOffice/BoxOfficeViewController.swift
+++ BoxOffice/Views/BoxOffice/BoxOfficeViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class BoxOfficeViewController: UIViewController {
+    
+    let boxOfficeView: BoxOfficeView = {
+        let view = BoxOfficeView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        addSubView()
+        configure()
     }
-
-
+    
+    func addSubView() {
+        view.addSubview(boxOfficeView)
+    }
+    
+    func configure() {
+        NSLayoutConstraint.activate([
+            boxOfficeView.topAnchor.constraint(equalTo: view.topAnchor),
+            boxOfficeView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            boxOfficeView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            boxOfficeView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 }
 
