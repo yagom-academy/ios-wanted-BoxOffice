2edffcb4c3606dcf94457b4495bb05b7c5cc2de9
SChan-Kim <instufld@gmail.com>
2022-10-19 02:17:02 +0900

    refactor: 코드로만 UI 구성, 폴더 구조 변경

--- BoxOffice/Presentation/MoviesScene/MoviesList/View/MoviesListViewController.swift
+++ BoxOffice/Presentation/MoviesScene/MoviesList/View/MoviesListViewController.swift
@@ -1,19 +1,18 @@
 //
-//  ViewController.swift
+//  MoviesListViewController.swift
 //  BoxOffice
 //
-//  Created by kjs on 2022/10/14.
+//  Created by channy on 2022/10/19.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class MoviesListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
-
