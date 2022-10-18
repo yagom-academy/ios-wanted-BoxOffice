f4d9ff17003a0b9b538a3a72f54e5fb7f539b52a
swc00057 <swc00057@naver.com>
2022-10-18 14:35:05 +0900

    add: BoxOfficeViewController

--- BoxOffice/SceneDelegate.swift
+++ BoxOffice/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let mainVC = ViewController()
+        let mainVC = BoxOfficeViewController()
         let navigationController = UINavigationController(rootViewController: mainVC)
         window?.rootViewController = navigationController
         window?.backgroundColor = .systemBackground
