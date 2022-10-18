c468dbd95ea64166ea5f3cd678dea1694e3c8128
shadow9503 <33388081+shadow9503@users.noreply.github.com>
2022-10-18 17:30:03 +0900

    docs: Rename Main VC
    - 및 그룹으로 이동

---
+++ BoxOffice/View/MovieListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListViewController.swift
+//  BoxOffice
+//
+//  Created by kjs on 2022/10/14.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()    }
+
+
+}
+
