5ffa70d71673968e27085fd6d57fb5816c2797e6
shadow9503 <33388081+shadow9503@users.noreply.github.com>
2022-10-19 16:10:29 +0900

    feat: 네이바 숨김처리 at MovielistVC

--- BoxOffice/View/MovieList/View/MovieListViewController.swift
+++ BoxOffice/View/MovieList/View/MovieListViewController.swift
@@ -69,6 +69,7 @@ extension MovieListViewController: DefaultViewSetupProtocol {
     func configureView() {
         movieListTableView.backgroundColor = UIColor(named: "backgroundColorAsset")
         movieListTableView.separatorInset = UIEdgeInsets(top: 0, left: -10, bottom: 0, right: -10)
+        navigationController?.navigationBar.isHidden = true;
     }
 }
 
