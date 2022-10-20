196d6defa3da26755a3ba6c5f00aab1668c3905d
betterhee <betterhee@gmail.com>
2022-10-20 12:02:10 +0900

    chore: Add package dependency for FirebaseFirestore, FirebaseStorage

--- BoxOffice/Application Delegate/AppDelegate.swift
+++ BoxOffice/Application Delegate/AppDelegate.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseStorage
+import FirebaseFirestore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        FirebaseApp.configure()
+        let storage = Storage.storage()
+        let db = Firestore.firestore()
+        
         return true
     }
 
@@ -30,7 +35,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
 
 }
 
