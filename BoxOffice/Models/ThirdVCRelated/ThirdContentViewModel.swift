aa8910e827d8b59f1c40fa31f17d3557f9cb7401
JunYoungJee <rainmaker8492@gmail.com>
2022-10-22 16:18:04 +0900

    세번째 화면에 필요한 사항, 뷰 추가 중

--- BoxOffice/Models/ThirdVCRelated/ThirdContentViewModel.swift
+++ BoxOffice/Models/ThirdVCRelated/ThirdContentViewModel.swift
@@ -8,5 +8,18 @@
 import Foundation
 
 class ThirdContentViewModel: ObservableObject {
+    //input
     
+    //output
+    @Published var photo: String = ""
+    @Published var nickName: String = "닉네임닉네임"
+    @Published var passcode: String = "패스코드패스코드"
+    @Published var starRate: String = "스타레이트"
+    @Published var reviewContent: String = "리뷰컨텐트컨텐트"
+    
+    //properties
+    
+    init() {
+        
+    }
 }
--- BoxOffice/Views/ThirdVCRelated/ThirdContentView.swift
+++ BoxOffice/Views/ThirdVCRelated/ThirdContentView.swift
@@ -16,8 +16,31 @@ struct PhotoView: View {
 
 struct TextFieldView: View {
     
+    @Binding var viewTitle: String
+    @Binding var textInput: String
+    
     var body: some View {
-        TextField("타이틀", text: .constant("텍스트"))
+        HStack {
+            Text(viewTitle)
+            TextField("타이틀", text: $textInput)
+                .autocorrectionDisabled(true)
+                .textFieldStyle(.roundedBorder)
+        }
+        
+    }
+}
+
+struct TextView: UIViewRepresentable {
+    
+    typealias UIViewType = UITextView
+    var configuration = { (view: UIViewType) in }
+    
+    func makeUIView(context: UIViewRepresentableContext<Self>) -> UIViewType {
+        UIViewType()
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: UIViewRepresentableContext<Self>) {
+        configuration(uiView)
     }
 }
 
@@ -34,13 +57,13 @@ struct ThirdContentView: View {
     var body: some View {
         VStack(alignment: .center) {
             PhotoView()
-            TextFieldView()
+            TextFieldView(viewTitle: .constant("닉네임"), textInput: $viewModel.nickName)
                 .frame(minHeight: 40)
-            TextFieldView()
+            TextFieldView(viewTitle: .constant("암호"), textInput: $viewModel.passcode)
                 .frame(minHeight: 60)
             StarRateView()
                 .frame(minHeight: 80)
-            TextFieldView()
+            TextFieldView(viewTitle: .constant("리뷰내용"), textInput: $viewModel.reviewContent)
                 .frame(minHeight: 40)
         }.padding()
     }
