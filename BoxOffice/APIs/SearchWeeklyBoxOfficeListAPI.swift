cabad6a188521c2f9105d362427db7cdba1637f8
wongbingg <wb277@naver.com>
2023-01-02 21:59:06 +0900

    feat: SearchWeeklyBoxOfficeListAPI 생성 #4
    - date 날짜에 해당하는 주간/ 주말 박스오피스 목록을 받아오는 API 생성

---
+++ BoxOffice/APIs/SearchWeeklyBoxOfficeListAPI.swift
@@ -0,0 +1,38 @@
+//
+//  SearchWeeklyBoxOfficeListAPI.swift
+//  BoxOffice
+//
+//  Created by 이원빈 on 2023/01/02.
+//
+
+import Foundation
+
+enum WeekOption: String {
+    case allWeek = "0"
+    case weekEnd = "1"
+    case weekDays = "2"
+}
+
+struct SearchWeeklyBoxOfficeListAPI: API {
+    typealias ResponseType = WeeklyBoxOfficeResponseDTO
+    
+    var configuration: APIConfiguration
+    
+    init(date: String, itemPerPage: String = "10", weekOption: WeekOption) {
+        
+        self.configuration = APIConfiguration(
+            baseUrl: .kobis,
+            param: ["key": Bundle.main.kobisApiKey,
+                    "targetDt": date,
+                    "weekGb": weekOption.rawValue,
+                    "wideAreaCd": "0105001",
+                    "itemPerPage": itemPerPage],
+            path: .weeklyBoxOffice
+        )
+    }
+}
+
+struct WeeklyBoxOfficeResponseDTO: Decodable {
+    let boxOfficeResult: BoxOfficeResult
+}
+
