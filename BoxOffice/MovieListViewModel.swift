f8a937296fe2e530a2e7f617c8b17c538a939093
Neph <tony3322@naver.com>
2023-01-02 15:52:51 +0900

    [Feat] #2 viewModel이 movieList를 viewDidLoad 시점에 fetch해오는 로직 제작

--- BoxOffice/MovieListViewModel.swift
+++ BoxOffice/MovieListViewModel.swift
@@ -9,4 +9,21 @@ import Foundation
 
 final class MovieListViewModel {
     private let fetchMovieListUseCase = FetchMovieListUseCase()
+    private var pageToLoad = 0
+    var movieOverviewList = [MovieOverview]()
+    var dayType: DayType = .weekdays
+
+    var reloadMovieListTableView: (() -> Void)?
+
+    func viewDidLoad() {
+        fetchMovieListUseCase.execute(page: pageToLoad, dayType: dayType) { [weak self] result in
+            switch result {
+            case .success(let movieOverviewList):
+                self?.movieOverviewList = movieOverviewList
+                self?.reloadMovieListTableView?()
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
