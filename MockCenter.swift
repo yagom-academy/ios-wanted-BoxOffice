0e119de29dcfadf6a79a3e897685ef4bd76882ef
Neph <tony3322@naver.com>
2023-01-02 19:52:50 +0900

    [Feat] #1 MockCenter에 인터페이스에 맞게 구현 추가, FetchMoviewReviewUseCase 제작

--- MockCenter.swift
+++ MockCenter.swift
@@ -11,4 +11,12 @@ final class MockMovieDetailRepository: MovieDetailRepositoryInterface {
     func fetchMovieDetail(of movieCode: String, completion: @escaping (Result<MovieDetail, Error>) -> Void) {
         
     }
+
+    func fetchMovieReview(movieCode: String, completion: @escaping (Result<MovieReview, Error>) -> Void) {
+
+    }
+
+    func deleteMovieReview(review: MovieReview, completion: @escaping (Result<Void, Error>) -> Void) {
+
+    }
 }
