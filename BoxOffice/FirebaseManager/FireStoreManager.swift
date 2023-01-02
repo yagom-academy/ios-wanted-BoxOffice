3a2eb3af61cb814bec7c44e90643bf62bc901b0d
Judy <joo00ee@naver.com>
2023-01-02 23:59:14 +0900

    feat: FirebaseManager를 Singleton으로 구현 #3

---
+++ BoxOffice/FirebaseManager/FireStoreManager.swift
@@ -0,0 +1,53 @@
+//
+//  FireStoreManager.swift
+//  BoxOffice
+//
+//  Created by Judy on 2023/01/02.
+//
+
+import UIKit
+import FirebaseFirestore
+
+class FireStoreManager {
+    static let shared = FireStoreManager()
+    private let database = Firestore.firestore()
+    private let deviceID: String
+    
+    private init() {
+        guard let deviceIdentifier = UIDevice.current.identifierForVendor?.uuidString else {
+            deviceID = ""
+            return
+        }
+        deviceID = deviceIdentifier
+    }
+    
+    func save(_ data: [String: Any], with id: String) {
+        database.collection(deviceID).document(id).setData(data) { error in
+            if let error = error {
+                print("Error writing document: \(error)")
+            }
+        }
+    }
+    
+    func delete(id: String) {
+        database.collection(deviceID).document(id).delete { error in
+            if let error = error {
+                print("Error removing document: \(error)")
+            }
+        }
+    }
+    
+    func fetch(completionHandler: @escaping ([QueryDocumentSnapshot]) -> Void) {
+        database.collection(deviceID).getDocuments { querySnapshot, error in
+            if let error = error {
+                print("Error fetching document: \(error)")
+                return
+            }
+            
+            if let documents = querySnapshot?.documents {
+                completionHandler(documents)
+            }
+        }
+    }
+}
+
