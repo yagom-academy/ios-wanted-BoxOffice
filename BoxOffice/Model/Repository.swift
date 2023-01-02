fbe051b38ae71fd6e2273882e70bd1d174d811b2
SooJi <sokoooool86@gmail.com>
2023-01-02 20:35:42 +0900

    feat: Repository객체에 api호출을 위한 fetch 메서드 구현 타입앨리어스로 Result타입 구현

--- BoxOffice/Model/Repository.swift
+++ BoxOffice/Model/Repository.swift
@@ -9,4 +9,30 @@ import Foundation
 
 final class Repository {
     
+    private let firstApiKey = "325ca562"
+    private let secondApiKey = ""
+    
+    private let apiUrl = "http://www.omdbapi.com/?i=tt3896198"
+    private let posterUrl = "http://img.omdbapi.com/?"
+    
+    typealias MovieResult = Result<Data, Error>
+    
+    func fetch(completion: @escaping (MovieResult) -> Void) {
+        let urlString = apiUrl + firstApiKey
+        guard let url = URL(string: urlString) else { return }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard error == nil else {
+                completion(.failure(error!))
+                return
+            }
+            guard let response = response as? HTTPURLResponse else {
+                completion(.failure(NSError(domain: "네트워킹 실패", code: -1)))
+                return
+            }
+            print("\(#function), \(response.statusCode)")
+            
+            guard let data = data else { return }
+            completion(.success(data))
+        }.resume()
+    }
 }
