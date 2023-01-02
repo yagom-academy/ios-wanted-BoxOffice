ff9ab69d6636c13ceb74c7754c9023829b6c2219
Judy <joo00ee@naver.com>
2023-01-03 00:02:09 +0900

    feat: StorageManager를 Singleton으로 구현 #3

---
+++ BoxOffice/FirebaseManager/StorageManager.swift
@@ -0,0 +1,55 @@
+//
+//  StorageManager.swift
+//  BoxOffice
+//
+//  Created by Judy on 2023/01/02.
+//
+
+import UIKit
+import FirebaseStorage
+
+class StorageManager {
+    static let shared = StorageManager()
+    private let storageReference = Storage.storage().reference()
+    private let deviceID: String
+    
+    private init() {
+        guard let deviceIdentifier = UIDevice.current.identifierForVendor?.uuidString else {
+            deviceID = ""
+            return
+        }
+        deviceID = deviceIdentifier
+    }
+    
+    func save(_ image: UIImage, id: String) {
+        guard let imageData = image.jpegData(compressionQuality: 1.0) else { return }
+        
+        storageReference.child("\(deviceID)/\(id)").putData(imageData, metadata: nil) { _, error in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func delete(id: String) {
+        storageReference.child("\(deviceID)/\(id)").delete { error in
+            if let error = error {
+                print("Error removing image: \(error)")
+            }
+        }
+    }
+    
+    func fetch(id: String, completion: @escaping (UIImage?) -> Void) {
+        let size: Int64 = 1 * 1024 * 1024
+        
+        storageReference.child("\(deviceID)/\(id)").getData(maxSize: size) { data, error in
+            if error != nil {
+                completion(nil)
+            }
+            
+            if let data = data {
+                completion(UIImage(data: data))
+            }
+        }
+    }
+}
