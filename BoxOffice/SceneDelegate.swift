1ec048cfd41bb2efade71908c614bf62870a185d
Judy <joo00ee@naver.com>
2023-01-02 17:31:25 +0900

    feat: info.plist 내의 Main 연결 삭제 #1

--- BoxOffice/SceneDelegate.swift
+++ BoxOffice/SceneDelegate.swift
@@ -11,7 +11,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        
+        let mainviewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: mainviewController)
+        
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
