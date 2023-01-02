5f9099c183fc154be2f71f870cd33e6dd9895cee
Neph <tony3322@naver.com>
2023-01-03 02:15:13 +0900

    [Feat] #4 MovieDetailViewModel에서 필요한 프로퍼티, 메서드 제작

--- BoxOffice/MovieDetailViewModel.swift
+++ BoxOffice/MovieDetailViewModel.swift
@@ -8,9 +8,81 @@
 import Foundation
 
 final class MovieDetailViewModel {
+
+    // MARK: - Outputs
     var tabBarMode: TabBarMode = .movieInfo
+    var movieDetail = dummyMovieDetail
+    var movieReviews = [MovieReview]()
+
+    // MARK: - UseCases
+    private let fetchMovieDetailUseCase = FetchMovieDetailUseCase()
+    private let fetchMovieReviewUseCase = FetchMovieReviewsUseCase()
+    private let deleteMovieReviewUseCase = DeleteMovieReviewUseCase()
+
+    // MARK: - Actions
+    var applyDataSource: (() -> Void)?
+
+    // MARK: - Private properties
+    private let movieCode: String
+
+    init(movieCode: String) {
+        self.movieCode = movieCode
+    }
 }
 
+// MARK: - Inputs
+extension MovieDetailViewModel {
+    func viewDidLoad() {
+        fetchMovieDetail(movieCode: movieCode)
+    }
+
+    func deleteReviewButtonTapped(review: MovieReview) {
+
+    }
+
+    func shareButtonTapped() {
+
+    }
+}
+
+// MARK: - Private functions
+extension MovieDetailViewModel {
+    private func fetchMovieDetail(movieCode: String) {
+        fetchMovieDetailUseCase.execute(movieCode: movieCode) { [weak self] result in
+            switch result {
+            case .success(let movieDetail):
+                self?.movieDetail = movieDetail
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+
+    private func fetchMovieReview(movieCode: String) {
+        fetchMovieReviewUseCase.execute(movieCode: movieCode) { [weak self] result in
+            switch result {
+            case .success(let reviews):
+                self?.movieReviews = reviews
+                self?.applyDataSource?()
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+
+    private func deleteMovieReview(review: MovieReview) {
+        deleteMovieReviewUseCase.execute(review: review) { result in
+            switch result {
+            case .success:
+                break
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
+
+// MARK: - TabBarMode
 extension MovieDetailViewModel {
     enum TabBarMode {
         case movieInfo
