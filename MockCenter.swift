3fe2def0ae2cd07561a09e78332fec7f324ddf9a
Neph <tony3322@naver.com>
2023-01-03 02:01:30 +0900

    [Feat] #4 Mock Repository에서 시간간격을 두고 dummy data를 completion handler에 전달하도록 제작

--- MockCenter.swift
+++ MockCenter.swift
@@ -5,29 +5,56 @@
 //  Created by minsson on 2023/01/02.
 //
 
-import Foundation
+import UIKit
 
 final class MockMovieListRepository: MovieListRepositoryInterface {
     func fetchMovieList(page: Int, dayType: DayType, completion: @escaping (Result<[MovieOverview], Error>) -> Void) {
+        let overviews = Array.init(repeating: dummyMovieOverview, count: 20)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            completion(.success(overviews))
+        }
     }
 }
 
 final class MockMovieDetailRepository: MovieDetailRepositoryInterface {
     func fetchMovieDetail(movieCode: String, completion: @escaping (Result<MovieDetail, Error>) -> Void) {
-        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            completion(.success(dummyMovieDetail))
+        }
     }
 
     func fetchMovieReview(movieCode: String, completion: @escaping (Result<[MovieReview], Error>) -> Void) {
-
+        let reviews = Array.init(repeating: dummyMovieReview, count: 20)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            completion(.success(reviews))
+        }
     }
 
     func deleteMovieReview(review: MovieReview, completion: @escaping (Result<Void, Error>) -> Void) {
-
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            completion(.success(()))
+        }
     }
 }
 
 final class MockReviewWritingRepository: ReviewWritingRepositoryInterface {
     func uploadReview(review: MovieReview, completion: @escaping (Result<Void, Error>) -> Void) {
-
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            completion(.success(()))
+        }
     }
 }
+
+var dummyMovieOverview: MovieOverview {
+    return MovieOverview(movieCode: "", dayType: .weekdays, region: .Seoul, rank: 1, title: "아바타", openingDay: Date(), audienceNumber: 200, rankFluctuation: 1, isNewlyRanked: true)
+}
+
+var dummyMovieDetail: MovieDetail {
+    return MovieDetail(movieCode: UUID().description, rank: 1, title: "아바타", openingDay: Date(), audienceNumber: 10000, rankFluctuation: 1, isNewlyRanked: true, productionYear: 2023, playTime: 123.5, genre: "SF", directorsName: "봉준호", actorsName: "이병헌", watchGrade: 13) }
+
+var dummyMovieDetails: [MovieDetail] = Array.init(repeating: MovieDetail(movieCode: UUID().description, rank: 1, title: "아바타", openingDay: Date(), audienceNumber: 10000, rankFluctuation: 1, isNewlyRanked: true, productionYear: 2023, playTime: 123.5, genre: "SF", directorsName: "봉준호", actorsName: "이병헌", watchGrade: 13), count: 10)
+
+var dummyMovieReview: MovieReview {
+    return MovieReview(id: UUID(), user: User(id: UUID(), nickname: "Neph"), password: "1234", rating: 5, image: UIImage(systemName: "person")!)
+}
+
