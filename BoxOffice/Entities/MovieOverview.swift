03616db8af0aa9d2a1a4e5186ec10bfa0d8bfb08
Neph <tony3322@naver.com>
2023-01-02 14:09:37 +0900

    [Feat] #1 MovieOverview Entity 제작

---
+++ BoxOffice/Entities/MovieOverview.swift
@@ -0,0 +1,34 @@
+//
+//  MovieOverview.swift
+//  BoxOffice
+//
+//  Created by 천수현 on 2023/01/02.
+//
+import Foundation
+
+struct MovieOverview {
+    let dayType: DayType
+    let region: Region
+    let rank: UInt
+    let title: String
+    let openingDay: Date
+    let audienceNumber: UInt
+    let rankFluctuation: Int
+    let isNewlyRanked: Bool
+}
+
+enum DayType {
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+    case saturday
+    case sunday
+    case weekdays
+    case weekends
+}
+
+enum Region {
+    case Seoul
+}
