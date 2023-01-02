6d7e78734e752e86956274c6bf991721d8b10edf
Neph <tony3322@naver.com>
2023-01-02 22:40:08 +0900

    [Feat] #4 DiffableDataSource 생성, apply 메서드 제작

--- BoxOffice/MovieDetailViewController.swift
+++ BoxOffice/MovieDetailViewController.swift
@@ -23,8 +23,11 @@ final class MovieDetailViewController: UIViewController {
         collectionView.register(MovieDetailTabBarHeaderView.self,
                                 forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
                                 withReuseIdentifier: MovieDetailTabBarHeaderView.reuseIdentifier)
+        collectionView.dataSource = movieDetailDataSource
         return collectionView
     }()
+
+    private lazy var movieDetailDataSource = movieDetailCollectionViewDataSource()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,6 +43,34 @@ final class MovieDetailViewController: UIViewController {
             movieDetailCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             movieDetailCollectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
+    }
+
+    private func movieDetailCollectionViewDataSource() -> UICollectionViewDiffableDataSource<MovieDetailSection, MovieDetailItem> {
+        let dataSource = UICollectionViewDiffableDataSource<MovieDetailSection, MovieDetailItem>(
+            collectionView: movieDetailCollectionView
+        ) { collectionView, indexPath, itemIdentifier -> UICollectionViewCell? in
+            return UICollectionViewCell()
+        }
+        dataSource.supplementaryViewProvider = { collectionView, kind, indexPath in
+            guard kind == UICollectionView.elementKindSectionHeader,
+                  let header = collectionView.dequeueReusableSupplementaryView(
+                    ofKind: kind,
+                    withReuseIdentifier: MovieDetailTabBarHeaderView.reuseIdentifier,
+                    for: indexPath
+                  ) as? MovieDetailTabBarHeaderView else { return UICollectionReusableView() }
+
+            return header
+        }
+
+        return dataSource
+    }
+
+    private func applyDataSource() {
+        var snapShot = NSDiffableDataSourceSnapshot<MovieDetailSection, MovieDetailItem>()
+        snapShot.appendSections([.upper, .bottom])
+        snapShot.appendItems([], toSection: .upper)
+        snapShot.appendItems([], toSection: .bottom)
+        movieDetailDataSource.apply(snapShot)
     }
 
     private func movieDetailCollectionViewLayout() -> UICollectionViewLayout {
@@ -59,6 +90,16 @@ final class MovieDetailViewController: UIViewController {
         }, configuration: configuration)
 
         return compositionalLayout
+    }
+}
+
+// MARK: - MoviewDetailDiffableDataSource
+extension MovieDetailViewController {
+    /// Wrapper Enum to use DiffableDataSource
+    enum MovieDetailItem: Hashable {
+        case upper(MovieDetail)
+        case info(MovieDetail)
+        case review(MovieReview)
     }
 }
 
