d1900841b8cb3850211bc22bce1246f9697f6d89
brad <gjrjs31@naver.com>
2023-01-03 10:56:17 +0900

    refactor: SwiftUI로 변경

---
+++ BoxOffice/BoxOffice/BoxOfficeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxOfficeApp.swift
+//  BoxOffice
+//
+//  Created by brad on 2023/01/03.
+//
+
+import SwiftUI
+
+@main
+struct BoxOfficeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
