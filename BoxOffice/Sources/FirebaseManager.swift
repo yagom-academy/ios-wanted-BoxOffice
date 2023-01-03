9bead1c4b2a45a37b2b4488ce1cff8bdd386077a
yeeton37 <lyeeun37@naver.com>
2023-01-03 12:07:54 +0900

    feat:  FirebaseManager 생성

---
+++ BoxOffice/Sources/FirebaseManager.swift
@@ -0,0 +1,18 @@
+//
+//  FirebaseManager.swift
+//  BoxOffice
+//
+//  Created by 이예은 on 2023/01/03.
+//
+
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+final class FirebaseManager {
+    
+    func save() {}
+    
+    func subscribe() {}
+}
+
+
