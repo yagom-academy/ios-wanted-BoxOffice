342b5e2c9d989c591437c3b83e61a19d13b04bca
lcy <pgbenny0035@gmail.com>
2023-01-03 14:23:50 +0900

    feat: 서버통신 함수 구현

---
+++ BoxOffice/Network.swift
@@ -0,0 +1,49 @@
+//
+//  Network.swift
+//  BoxOffice
+//
+//  Created by 임채윤 on 2023/01/03.
+//
+
+import Foundation
+
+class Network {
+    
+}
+
+extension Network {
+    func getData<T: Decodable>(url: String, completion: @escaping (Result<T, Error>) -> Void) {
+        guard let url = URL(string: url) else {
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                return
+            }
+            
+            guard (200...299).contains(response.statusCode) else {
+                return
+            }
+            
+            if let data = data {
+                do {
+                    let data = try JSONDecoder().decode(T.self, from: data)
+                    completion(.success(data))
+                } catch {
+                    completion(.failure(error))
+                }
+            }
+            
+            
+        }
+        task.resume()
+    }
+}
