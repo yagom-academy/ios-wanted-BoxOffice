f9a6c70bda0b9a2c0be8652dc5108d397119538c
yeeton37 <lyeeun37@naver.com>
2023-01-03 12:16:02 +0900

    feat: Review 모델 타입 생성
    - 사용자로부터 입력받은 리뷰에 대한 정보를 Review 타입에 매핑시키기 위해 생성

---
+++ BoxOffice/Sources/Review.swift
@@ -0,0 +1,15 @@
+//
+//  Review.swift
+//  BoxOffice
+//
+//  Created by 이예은 on 2023/01/03.
+//
+
+import Foundation
+
+struct Review: Codable {
+    let userImage: String
+    let stars: Double
+    let nickname: String
+    let password: String
+}
