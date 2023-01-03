b779ae80515a71d34cec65faabf32c4e63dd2081
Judy <joo00ee@naver.com>
2023-01-03 10:02:24 +0900

    feat: 리뷰 정보를 담는 Review 타입 생성 #3

---
+++ BoxOffice/Domain/Entities/Review.swift
@@ -0,0 +1,16 @@
+//
+//  Review.swift
+//  BoxOffice
+//
+//  Created by Judy on 2023/01/03.
+//
+
+import UIKit
+
+struct Review {
+    let nickName: String
+    let password: String
+    let rating: Double
+    let content: String
+    let photo: UIImage?
+}
