132dee08274f8a86da55a05ebf980f524771eb54
yeeton37 <lyeeun37@naver.com>
2023-01-03 13:08:00 +0900

    refactor: FirebaseManager 내부 메서드 수정

--- BoxOffice/Sources/FirebaseManager.swift
+++ BoxOffice/Sources/FirebaseManager.swift
@@ -9,33 +9,48 @@ import FirebaseFirestore
 import FirebaseFirestoreSwift
 
 final class FirebaseManager {
-    let db = Firestore.firestore()
+    private let collectionType: FirebaseModel
+    private let database = Firestore.firestore()
+    
+    init(collectionType: FirebaseModel) {
+        self.collectionType = collectionType
+    }
     
     func save(review: Review) {
-        db.collection("review").document(review.password).setData(review.asDictionary!)
+        guard let reviewDictionary = review.asDictionary else {
+            return
+        }
+        
+        database.collection("review").document(review.password).setData(reviewDictionary)
     }
     
     func update(review: Review) {
-        db.collection("review").document(review.password).setData(
-            review.asDictionary!,
+        guard let reviewDictionary = review.asDictionary else {
+            return
+        }
+        
+        database.collection("review").document(review.password).setData(
+            reviewDictionary,
             merge: true
         )
     }
     
     func delete(review: Review) {
-        db.collection("review").document(review.password).delete()
+        database.collection("review").document(review.password).delete()
     }
     
     func fetchAll() {
-        db.collection("review").getDocuments { (snapshot, error) in
+        database.collection("review").getDocuments { (snapshot, error) in
             if error == nil && snapshot != nil {
-                for document in snapshot!.documents {
+                guard let snapshot = snapshot else {
+                    return
+                }
+                
+                for document in snapshot.documents {
                     print(document.documentID)
                     print(document.data())
                 }
-            } else {
-                
-            }
+            } 
         }
     }
 }
