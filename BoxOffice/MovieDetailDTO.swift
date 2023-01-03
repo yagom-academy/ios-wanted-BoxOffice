c4028de38b9da874165e50c2f5380bd786029cee
minsson <mskaang@gmail.com>
2023-01-03 17:32:40 +0900

    [Fix] #5 MovieDetailDTO 프로퍼티의 모든 속성을 String으로 수정
    - 서버 API 응답의 타입이 모두 문자열이므로 이에 맞추어 String으로 수정함

--- BoxOffice/MovieDetailDTO.swift
+++ BoxOffice/MovieDetailDTO.swift
@@ -11,12 +11,12 @@ struct MovieDetailDTO: Decodable {
     let movieCode: String
     let title: String
     let openingDay: String
-    let productionYear: UInt
-    let playTime: Double
+    let productionYear: String
+    let playTime: String
     let genre: String
     let directorsName: String
     let actorsName: String
-    let watchGrade: UInt
+    let watchGrade: String
 
     enum CodingKeys: String, CodingKey {
         case movieCode = "movieCd"
