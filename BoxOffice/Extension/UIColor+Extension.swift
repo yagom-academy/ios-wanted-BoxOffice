c0b734e779301c356fea3145bd79cc42aa0181d4
jcrescent61 <jcrescent61@gmail.com>
2023-01-03 21:52:19 +0900

    feat: UIColor Extension 구현

--- BoxOffice/Extension/UIColor+Extension.swift
+++ BoxOffice/Extension/UIColor+Extension.swift
@@ -5,4 +5,10 @@
 //  Created by Ellen J on 2023/01/03.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat = 1.0) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: a)
+    }
+}
