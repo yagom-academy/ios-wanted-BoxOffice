63dfa56b267ad067eaec134a9da7683fc0849029
dddowon <dowon4095@naver.com>
2023-01-03 13:48:02 +0900

    feat: DailyBoxOffice 모델 구현

---
+++ BoxOffice/Model/DailyBoxOffice.swift
@@ -0,0 +1,13 @@
+//
+//  DayBoxOffice.swift
+//  BoxOffice
+//
+//  Created by 박도원 on 2023/01/03.
+//
+
+import Foundation
+
+struct DailyBoxOffice: Decodable {
+    let boxofficeType: String
+    let dailyBoxOfficeList: [DailyBoxOfficeList]
+}
---
+++ BoxOffice/Model/DailyBoxOfficeList.swift
@@ -0,0 +1,18 @@
+//
+//  DailyBoxOfficeList.swift
+//  BoxOffice
+//
+//  Created by 박도원 on 2023/01/03.
+//
+
+import Foundation
+
+struct DailyBoxOfficeList: Decodable {
+    let rank: String
+    let rankInten: String
+    let rankOldAndNew: String
+    let movieCd: String
+    let movieNm: String
+    let openDt: String
+    let audiAcc: String
+}
