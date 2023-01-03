1a2ce6070623c77e60ba595f58060f68a3203f65
jcrescent61 <jcrescent61@gmail.com>
2023-01-03 12:26:27 +0900

    feat: DailyBoxOffice 모델타입 구현

---
+++ BoxOffice/Model/DailyBoxOffice.swift
@@ -0,0 +1,28 @@
+//
+//  DailyBoxOffice.swift
+//  BoxOffice
+//
+//  Created by Ellen J on 2023/01/03.
+//
+
+import Foundation
+
+struct DailyBoxOffice: Decodable {
+    let movieCode: String?
+    let rank: String?
+    let title: String?
+    let openDate: String?
+    let audienceCount: String?
+    let rankInTen: String?
+    let isNewRank: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case movieCode = "movieCd"
+        case rank = "rank"
+        case title = "movieNm"
+        case openDate = "openDt"
+        case audienceCount = "audiCnt"
+        case rankInTen = "rankInten"
+        case isNewRank = "rankOldAndNew"
+    }
+}
