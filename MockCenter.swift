4726f66ce0399dd695dc7bea1c76f69889a7f6d1
minsson <mskaang@gmail.com>
2023-01-04 20:09:28 +0900

    [Feat] #2 MockMovieListRepository에 MockData 생성
    - 네트워킹이 없는 상태에서 프레젠테이션 레이어 쪽에 필요한 데이터를 받아오기 위함

--- MockCenter.swift
+++ MockCenter.swift
@@ -9,6 +9,15 @@ import Foundation
 
 final class MockMovieListRepository: MovieListRepositoryInterface {
     func fetchMovieList(page: Int, dayType: DayType, completion: @escaping (Result<[MovieOverview], Error>) -> Void) {
+        let movieOverviewList = [
+            MovieOverview(movieCode: "1", dayType: .weekdays, region: .Seoul, rank: 1, title: "임시 TITLE 1", openingDay: Date(), audienceNumber: 10000, rankFluctuation: 3, isNewlyRanked: true),
+            MovieOverview(movieCode: "2", dayType: .weekdays, region: .Seoul, rank: 2, title: "임시 TITLE 2", openingDay: Date(), audienceNumber: 1000, rankFluctuation: -1, isNewlyRanked: false),
+            MovieOverview(movieCode: "3", dayType: .weekdays, region: .Seoul, rank: 3, title: "임시 TITLE 3", openingDay: Date(), audienceNumber: 100, rankFluctuation: 0, isNewlyRanked: false),
+            MovieOverview(movieCode: "4", dayType: .weekdays, region: .Seoul, rank: 4, title: "임시 TITLE 4", openingDay: Date(), audienceNumber: 100, rankFluctuation: 5, isNewlyRanked: true),
+            MovieOverview(movieCode: "5", dayType: .weekdays, region: .Seoul, rank: 5, title: "임시 TITLE 5", openingDay: Date(), audienceNumber: 100, rankFluctuation: 5, isNewlyRanked: false)
+        ]
+        
+        completion(.success(movieOverviewList))
     }
 }
 
