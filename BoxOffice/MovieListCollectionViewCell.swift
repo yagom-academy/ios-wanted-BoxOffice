1d0b9b9d5649d73a7dd264f86efed6d3313cb52c
minsson <mskaang@gmail.com>
2023-01-04 17:10:49 +0900

    [Feat] #2 영화목록 셀 UIComponent 1차 생성
    - 추후 폰트 등 세부 조정 예정

--- BoxOffice/MovieListCollectionViewCell.swift
+++ BoxOffice/MovieListCollectionViewCell.swift
@@ -14,6 +14,84 @@ final class MovieListCollectionViewCell: UICollectionViewCell {
     private var movieCode: String?
     private var dayType: DayType?
     private var region: Region?
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(systemName: "image.fill")
+        imageView.backgroundColor = .systemGray4
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.cornerRadius = 10
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .boldSystemFont(ofSize: 16)
+        return label
+    }()
+        
+    private let rankValueLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .systemGray
+        return label
+    }()
+    
+    private let openingDayLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "개봉일"
+        label.textColor = .secondaryLabel
+        return label
+    }()
+    
+    private let openingDayValueLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .systemGray
+        return label
+    }()
+    
+    private let audienceNumberLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "관객수"
+        label.textColor = .secondaryLabel
+        return label
+    }()
+    
+    private let audienceNumberValueLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .systemGray
+        return label
+    }()
+    
+    private let rankFluctuationLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "순위변동"
+        label.textColor = .secondaryLabel
+        return label
+    }()
+    
+    private let rankFluctuationValueLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .red
+        return label
+    }()
+    
+    private let newlyRankedLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .red
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
