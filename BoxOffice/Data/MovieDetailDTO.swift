180b576d34d80fbf99020da10af4f5672906b4f6
Neph <tony3322@naver.com>
2023-01-05 01:20:44 +0900

    [Feat] #8 MovieDetailDTO toDomain 제작

--- BoxOffice/Data/MovieDetailDTO.swift
+++ BoxOffice/Data/MovieDetailDTO.swift
@@ -10,6 +10,7 @@ import Foundation
 struct MovieDetailDTO: Decodable {
     let movieCode: String
     let title: String
+    let englishTitle: String
     let openingDay: String
     let productionYear: String
     let playTime: String
@@ -20,7 +21,8 @@ struct MovieDetailDTO: Decodable {
 
     enum CodingKeys: String, CodingKey {
         case movieCode = "movieCd"
-        case title = "movieNmEn"
+        case title = "movieNm"
+        case englishTitle = "movieNmEn"
         case openingDay = "openDt"
         case productionYear = "prdtYear"
         case playTime = "showTm"
@@ -28,5 +30,37 @@ struct MovieDetailDTO: Decodable {
         case directorsName = "directors"
         case actorsName = "actors"
         case watchGrade = "watchGradeNm"
+    }
+}
+
+extension MovieDetailDTO {
+    func toDomain() -> MovieDetail {
+        return MovieDetail(
+            movieCode: movieCode,
+            rank: 0,
+            title: title,
+            englishTitle: englishTitle,
+            openingDay: openingDay.toDate(),
+            audienceNumber: 0,
+            rankFluctuation: 0,
+            isNewlyRanked: false,
+            productionYear: UInt(productionYear) ?? 0,
+            playTime: Double(playTime) ?? 0,
+            genre: genre,
+            directorsName: directorsName,
+            actorsName: actorsName,
+            watchGrade: UInt(watchGrade) ?? 0,
+            posterImageURL: "") // TODO: 여기서 fetch하기
+    }
+}
+
+fileprivate extension String {
+    func toDate() -> Date {
+        let formatter = DateFormatter()
+        guard let date = formatter.date(from: self) else {
+            return Date()
+        }
+
+        return date
     }
 }
