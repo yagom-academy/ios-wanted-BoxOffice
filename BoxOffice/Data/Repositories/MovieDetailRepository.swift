35f8a5a7e796577d179329426b0934b0ef737b03
Neph <tony3322@naver.com>
2023-01-05 01:54:55 +0900

    [Feat] #8 MovieDetailFetch 작업 구현, mock에서 실구현부로 변경

--- BoxOffice/Data/Repositories/MovieDetailRepository.swift
+++ BoxOffice/Data/Repositories/MovieDetailRepository.swift
@@ -10,8 +10,33 @@ import Foundation
 final class MovieDetailRepository: MovieDetailRepositoryInterface {
 
     private let firebaseService = FirebaseService.shared
+    private let neworkService = NetworkService.shared
 
     func fetchMovieDetail(movieCode: String, completion: @escaping (Result<MovieDetail, Error>) -> Void) {
+        var urlComponents = URLComponents(string: "https://www.kobis.or.kr")
+        urlComponents?.path = "/kobisopenapi/webservice/rest/movie/searchMovieInfo"
+        urlComponents?.queryItems = [
+            .init(name: "key", value: neworkService.koficAPIKey),
+            .init(name: "movieCd", value: movieCode)
+        ]
+        guard let url = urlComponents?.url else { return }
+        let urlRequest = URLRequest(url: url)
+        let task = neworkService.dataTask(request: urlRequest) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let movieDetailResponseDTO = try JSONDecoder().decode(MovieDetailResponseDTO.self, from: data)
+                    completion(.success(movieDetailResponseDTO.movieInfoResult.movieInfo.toDomain()))
+                } catch {
+                    completion(.failure(error))
+                }
+                break
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+
+        task.resume()
     }
 
     func fetchMovieReview(movieCode: String, completion: @escaping (Result<[MovieReview], Error>) -> Void) {
@@ -30,4 +55,9 @@ final class MovieDetailRepository: MovieDetailRepositoryInterface {
 
     func deleteMovieReview(review: MovieReview, completion: @escaping (Result<Void, Error>) -> Void) {
     }
+}
+
+// MARK: - Private Functions
+extension MovieDetailRepository {
+
 }
