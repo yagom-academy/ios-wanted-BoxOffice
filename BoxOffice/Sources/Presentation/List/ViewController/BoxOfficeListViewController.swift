b8d53a2c5c7720e4b6542c08f683227f1cb62b91
Ari <lee_ari95@icloud.com>
2023-01-04 16:56:09 +0900

    feat: 메인 화면에 세그먼트 컨트롤과 테이블뷰를 추가

--- BoxOffice/Sources/Presentation/List/ViewController/BoxOfficeListViewController.swift
+++ BoxOffice/Sources/Presentation/List/ViewController/BoxOfficeListViewController.swift
@@ -12,10 +12,23 @@ class BoxOfficeListViewController: UIViewController {
     weak var coordinator: BoxOfficeListCoordinatorInterface?
     private let viewModel: BoxOfficeListViewModel
     
+    private lazy var segmentedControl: UISegmentedControl = {
+        let control = UISegmentedControl(items: ["일간", "주간", "주말"])
+        control.selectedSegmentIndex = 0
+        control.addTarget(self, action: #selector(didTapSegmented(_:)), for: .valueChanged)
+        return control
+      }()
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.rowHeight = 100
+        tableView.backgroundColor = .clear
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .boBackground
+        setUp()
     }
 
     init(viewModel: BoxOfficeListViewModel, coordinator: BoxOfficeListCoordinatorInterface) {
@@ -30,3 +43,46 @@ class BoxOfficeListViewController: UIViewController {
     
 }
 
+private extension BoxOfficeListViewController {
+    
+    func setUp() {
+        setUpNavigationBar()
+        setUpView()
+    }
+    
+    func setUpNavigationBar() {
+        navigationItem.title = "BoxOffice"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func setUpView() {
+        view.backgroundColor = .boBackground
+        view.addSubviews(segmentedControl, tableView)
+        NSLayoutConstraint.activate([
+            segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            segmentedControl.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            segmentedControl.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            segmentedControl.heightAnchor.constraint(equalToConstant: 30),
+            tableView.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+    
+    @objc func didTapSegmented(_ sender: UISegmentedControl) {
+        print(sender.selectedSegmentIndex)
+    }
+}
+
+extension BoxOfficeListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+}
