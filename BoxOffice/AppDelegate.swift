809a604e9d7730c367c1a7266fc314b20aabe34c
Neph <tony3322@naver.com>
2023-01-04 23:30:00 +0900

    [Fix] Firbase configure를 viewcontroller init보다 먼저하도록 변경

--- BoxOffice/AppDelegate.swift
+++ BoxOffice/AppDelegate.swift
@@ -12,11 +12,11 @@ import FirebaseCore
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         window = UIWindow(frame: UIScreen.main.bounds)
         let rootViewController = ViewController()
         window?.rootViewController = rootViewController
         window?.makeKeyAndVisible()
-        FirebaseApp.configure()
         return true
     }
 }
