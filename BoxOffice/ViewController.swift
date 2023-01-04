8fc911616ac56de433da785d54bb071e8a74cddb
BaekGom <jihunmm02@gmail.com>
2023-01-04 15:05:43 +0900

    feat: 영화상세보기 화면 구현

--- BoxOffice/ViewController.swift
+++ BoxOffice/ViewController.swift
@@ -8,13 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let movieDetailView: SecondView = {
+        let view = SecondView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let contentScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.backgroundColor = .white
+        return scrollView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         view.backgroundColor = .systemBackground
+        configureUI()
     }
 
-
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
+extension ViewController {
+    private func configureUI() {
+        self.view.addSubview(self.movieDetailView)
+        setUpBaseUIConstraints()
+    }
+    
+    private func setUpBaseUIConstraints() {
+        movieDetailView.insetsLayoutMarginsFromSafeArea = false
+        
+        NSLayoutConstraint.activate([
+            self.movieDetailView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            self.movieDetailView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.movieDetailView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            self.movieDetailView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+        ])
+    }
+}
