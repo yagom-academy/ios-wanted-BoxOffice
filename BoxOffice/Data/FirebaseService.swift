f48c8d4ce7feed372224ccaa70c1b1adcf29ba4e
Neph <tony3322@naver.com>
2023-01-04 22:08:28 +0900

    [Feat] #8 firebase에서 review fetch하는 로직 제작

--- BoxOffice/Data/FirebaseService.swift
+++ BoxOffice/Data/FirebaseService.swift
@@ -9,11 +9,35 @@ import Foundation
 import FirebaseCore
 import FirebaseFirestore
 
+enum FirebaseError: Error {
+    case noData
+    case internalError
+}
+
 final class FirebaseService {
 
     static let shared = FirebaseService()
-
     private let database = Firestore.firestore()
+    private lazy var movieReviewReference = database.collection("movieReview")
 
     private init() { }
+
+    func fetchMovieReviews(of movieCode: String, completion: @escaping (Result<[MovieReviewDTO], FirebaseError>) -> Void) {
+        movieReviewReference.whereField("movieCode", isEqualTo: movieCode)
+            .getDocuments { snapShot, error in
+                guard error == nil else {
+                    completion(.failure(FirebaseError.internalError))
+                    return
+                }
+                guard let snapShot = snapShot else {
+                    completion(.failure(FirebaseError.noData))
+                    return
+                }
+
+                let data = snapShot.documents.compactMap {
+                    MovieReviewDTO(reviewData: $0.data())
+                }
+                completion(.success(data))
+            }
+    }
 }
