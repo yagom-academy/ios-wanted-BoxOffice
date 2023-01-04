29adfbdf7ae20a3246b5b884ff2969109b0efc12
Neph <tony3322@naver.com>
2023-01-04 23:03:03 +0900

    [Feat] #8 toDTO 로직 제작, Url이란 네이밍을 URL로 변경

--- BoxOffice/Data/MoviewReviewDTO.swift
+++ BoxOffice/Data/MoviewReviewDTO.swift
@@ -15,7 +15,7 @@ struct MovieReviewDTO {
         case userName
         case movieCode
         case rating
-        case imageUrl
+        case imageURL
         case password
         case description
     }
@@ -29,18 +29,26 @@ extension MovieReviewDTO {
               let movieCode = reviewData[Keys.movieCode.rawValue] as? String,
               let ratingString = reviewData[Keys.rating.rawValue] as? String,
               let rating = Double(ratingString),
-              let imageUrl = reviewData[Keys.imageUrl.rawValue] as? String,
+              let imageURL = reviewData[Keys.imageURL.rawValue] as? String,
               let password = reviewData[Keys.password.rawValue] as? String,
               let description = reviewData[Keys.description.rawValue] as? String else {
             return dummyMovieReview
         }
 
-        return MovieReview(id: id, movieCode: movieCode, user: User(nickname: userName), password: password, rating: rating, image: UIImage(), description: description) // TODO: Image Fetch
+        return MovieReview(id: id, movieCode: movieCode, user: User(nickname: userName), password: password, rating: rating, image: imageURL, description: description)
     }
 }
 
 extension MovieReview {
     func toDTO() -> MovieReviewDTO {
-        return MovieReviewDTO(reviewData: [:])
+        var reviewDictionary = [String: Any]()
+        reviewDictionary.updateValue(id.uuidString, forKey: "id")
+        reviewDictionary.updateValue(movieCode, forKey: "movieCode")
+        reviewDictionary.updateValue(user.nickname, forKey: "userName")
+        reviewDictionary.updateValue(password, forKey: "password")
+        reviewDictionary.updateValue(rating, forKey: "rating")
+        reviewDictionary.updateValue(image, forKey: "imageURL")
+        reviewDictionary.updateValue(description, forKey: "description")
+        return MovieReviewDTO(reviewData: reviewDictionary)
     }
 }
