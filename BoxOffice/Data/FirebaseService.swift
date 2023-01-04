6f7ecda23db04efe579db6451f882e083c077163
Neph <tony3322@naver.com>
2023-01-04 17:40:22 +0900

    [Feat] #8 Firebase 라이브러리 추가 및 FirebaseService 생성

---
+++ BoxOffice/Data/FirebaseService.swift
@@ -0,0 +1,21 @@
+//
+//  FirebaseService.swift
+//  BoxOffice
+//
+//  Created by 천수현 on 2023/01/04.
+//
+
+import Foundation
+import FirebaseCore
+import FirebaseFirestore
+
+final class FirebaseService {
+
+    static let shared = FirebaseService()
+
+    private let database = Firestore.firestore()
+
+    private init() {
+        FirebaseApp.configure()
+    }
+}
