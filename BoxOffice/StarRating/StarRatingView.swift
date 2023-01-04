39e1c4423606bb757b9652dcf740b761e3d43b27
Judy <joo00ee@naver.com>
2023-01-04 10:44:46 +0900

    chore: StarRatingUISlider를 파일과 그룹으로 분리

--- BoxOffice/StarRating/StarRatingView.swift
+++ BoxOffice/StarRating/StarRatingView.swift
@@ -126,18 +126,3 @@ class StarRatingView: UIView {
         }
     }
 }
-
-class StarRatingUISlider: UISlider {
-    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
-        let width = self.frame.size.width
-        let tapPoint = touch.location(in: self)
-        let tapPercent = tapPoint.x / width
-        let newValue = self.maximumValue * Float(tapPercent)
-        
-        if newValue != self.value {
-            self.value = newValue
-        }
-        
-        return true
-    }
-}
