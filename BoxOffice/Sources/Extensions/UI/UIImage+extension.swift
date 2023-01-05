8adeb97b3b77bd5f50cfc40ed51d955e55a478be
yeeton37 <lyeeun37@naver.com>
2023-01-05 21:08:16 +0900

    feat: 이미지를 firebase에 저장하기 위해 UIImage와 String 확장

---
+++ BoxOffice/Sources/Extensions/UI/UIImage+extension.swift
@@ -0,0 +1,16 @@
+//
+//  UIImage+extension.swift
+//  BoxOffice
+//
+//  Created by 이예은 on 2023/01/05.
+//
+
+import UIKit
+
+extension UIImage {
+    var toString: String? {
+        get {
+            return self.jpegData(compressionQuality: 1.0)?.base64EncodedString() ?? nil
+        }
+    }
+}
