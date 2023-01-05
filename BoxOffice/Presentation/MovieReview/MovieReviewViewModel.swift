90c60c15e37a3bff8ce2f9c7408bcff2475b4240
minsson <mskaang@gmail.com>
2023-01-06 06:07:39 +0900

    [Feat] #10 리뷰 필수 입력 사항이 모두 존재하는지 확인하는 메서드

--- BoxOffice/Presentation/MovieReview/MovieReviewViewModel.swift
+++ BoxOffice/Presentation/MovieReview/MovieReviewViewModel.swift
@@ -18,6 +18,7 @@ final class MovieReviewViewModel {
     // MARK: - Actions
     var presentImagePicker: (() -> Void)?
     var retrieveSelectedPhoto: (() -> Void)?
+    var presentViewController: ((UIViewController) -> Void)?
     
     func viewDidLoad() {
         
@@ -29,5 +30,26 @@ final class MovieReviewViewModel {
     
     func imageSelected(image: UIImage) {
         selectedPhoto = image
+    }
+    
+    func registrationButtonTapped(movieReview: MovieReview) {
+        if isValid(movieReview: movieReview) {
+            // TODO: upload review
+        } else {
+            let alert = UIAlertController(title: "빠진 정보가 있는 것 같아요.", message: "별점, 관람평, 닉네임, 패스워드를 모두 채워주셨나요?", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "확인", style: .cancel))
+            presentViewController?(alert)
+        }
+    }
+    
+    private func isValid(movieReview: MovieReview) -> Bool {
+        if movieReview.rating != 0,
+           movieReview.user.nickname != "",
+           movieReview.password != "",
+           movieReview.description != "" {
+            return true
+        }
+        
+        return false
     }
 }
