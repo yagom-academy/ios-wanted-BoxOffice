29ac31792951cbd7892bbb3616bdcca7a25b043e
wongbingg <wb277@naver.com>
2023-01-05 10:50:41 +0900

    feat: ModeSelectViewController의 CustomModal 생성 #6
    - coverVertical로 올라올 시, 높이를 커스텀
    - 좌측상단 x 버튼 탭시 dismiss
    - 아래로 스와이프 시 dismiss

---
+++ BoxOffice/ModeSelectModalView/ModeSelectPresentationController.swift
@@ -0,0 +1,111 @@
+//
+//  ModeSelectPresentationController.swift
+//  BoxOffice
+//
+//  Created by 이원빈 on 2023/01/04.
+//
+
+import UIKit
+
+final class ModeSelectPresentationController: UIPresentationController {
+    private var originalPosition: CGPoint?
+    private var currentPositionTouched: CGPoint?
+    private let dimmingView: UIView = {
+        let dimmingView = UIVisualEffectView(
+            effect: UIBlurEffect(style: .systemMaterialDark)
+        )
+        dimmingView.translatesAutoresizingMaskIntoConstraints = false
+        return dimmingView
+    }()
+
+    override func presentationTransitionWillBegin() {
+        super.presentationTransitionWillBegin()
+        
+        let superview = presentingViewController.view!
+        superview.addSubview(dimmingView)
+        setupDimmingViewLayout(in: superview)
+        adoptPanGestureRecognizer()
+        dimmingView.alpha = 0
+        
+        presentingViewController.transitionCoordinator?.animate(
+            alongsideTransition: { _ in
+            self.dimmingView.alpha = 0.5
+        })
+    }
+    
+    override func dismissalTransitionWillBegin() {
+        super.dismissalTransitionWillBegin()
+        
+        presentingViewController.transitionCoordinator?.animate(
+            alongsideTransition: { _ in
+            self.dimmingView.alpha = 0
+        }, completion: { _ in
+            self.dimmingView.removeFromSuperview()
+        })
+    }
+    
+    override var frameOfPresentedViewInContainerView: CGRect {
+        let size = CGSize(width: UIScreen.main.bounds.width,
+                          height: UIScreen.main.bounds.height/4)
+        let origin = CGPoint(x: 0, y: (UIScreen.main.bounds.height*3)/4)
+        return CGRect(origin: origin, size: size)
+    }
+
+    override init(presentedViewController: UIViewController,
+                  presenting presentingViewController: UIViewController?) {
+        super.init(presentedViewController: presentedViewController,
+                   presenting: presentingViewController)
+
+        presentedView?.autoresizingMask = [
+            .flexibleTopMargin,
+            .flexibleBottomMargin,
+            .flexibleLeftMargin,
+            .flexibleRightMargin
+        ]
+
+        presentedView?.translatesAutoresizingMaskIntoConstraints = true
+    }
+    
+    private func adoptPanGestureRecognizer() {
+        let adoptedView = containerView!
+        let panGestureRecognizer = UIPanGestureRecognizer(
+            target: self,
+            action: #selector(dismissView(_:))
+        )
+        adoptedView.addGestureRecognizer(panGestureRecognizer)
+    }
+    
+    private func setupDimmingViewLayout(in view: UIView) {
+        NSLayoutConstraint.activate([
+            dimmingView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            dimmingView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            dimmingView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            dimmingView.topAnchor.constraint(equalTo: view.topAnchor)
+        ])
+    }
+    
+    @objc private func dismissView(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: presentedView)
+        
+        if sender.state == .began {
+            originalPosition = presentedView!.center
+            currentPositionTouched = sender.location(in: presentedView)
+        } else if sender.state == .changed {
+            
+            if (presentedView?.frame.origin.y)! < 680 {
+                return
+            }
+            presentedView!.center.y = originalPosition!.y + translation.y*0.1
+        } else if sender.state == .ended {
+            let velocity = sender.velocity(in: presentedView)
+            
+            if velocity.y >= 100 {
+                presentedViewController.dismiss(animated: true)
+            } else {
+                UIView.animate(withDuration: 0.2) { [self] in
+                    presentedView?.center = originalPosition!
+                }
+            }
+        }
+    }
+}
