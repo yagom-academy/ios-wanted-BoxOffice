96b0f22f37cc386b9d4dc9cce468dfa6d3aa4418
minsson <mskaang@gmail.com>
2023-01-05 13:38:07 +0900

    [Feat] #10 MovieReviewViewController 생성

---
+++ BoxOffice/MovieReviewViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MovieReviewViewController.swift
+//  BoxOffice
+//
+//  Created by minsson on 2023/01/05.
+//
+
+import UIKit
+
+final class MovieReviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
