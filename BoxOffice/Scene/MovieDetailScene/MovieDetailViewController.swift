51f822c02622ca980704da7d528da7d973e49929
Judy <joo00ee@naver.com>
2023-01-05 09:03:40 +0900

    feat: MovieDetailViewController 생성 #10

---
+++ BoxOffice/Scene/MovieDetailScene/MovieDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MovieDetailViewController.swift
+//  BoxOffice
+//
+//  Created by Judy on 2023/01/04.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+    private enum Section {
+        case mainInfo, subInfo, review
+    }
+    
+    private let collectionView: UICollectionView? = nil
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
