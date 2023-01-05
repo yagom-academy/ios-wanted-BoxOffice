ff26cead7ddf425c165f555ac66f119b53ff1b52
wongbingg <wb277@naver.com>
2023-01-05 18:25:31 +0900

    refactor: API 사용 로직을 UseCase로 분리 #6
    https://github.com/Judy-999/ios-wanted-BoxOffice/pull/11#discussion_r1062073846

--- BoxOffice/HomeView/HomeViewModel.swift
+++ BoxOffice/HomeView/HomeViewModel.swift
@@ -8,120 +8,32 @@
 import Foundation
 
 protocol HomeViewModelInput {
-    var date: String? { get }
-    var itemPerPage: String? { get }
-    var weekOption: WeekOption? { get }
+    func requestDailyData(with date: String)
+    func requestAllWeekData(with date: String)
+    func requestWeekEndData(with date: String)
 }
 
 protocol HomeViewModelOutput{
-    var movieCellDatas: Observable<[MovieCellData]> { get }
-    func requestData()
+    var dailyMovieCellDatas: Observable<[MovieCellData]> { get }
+    var allWeekMovieCellDatas: Observable<[MovieCellData]> { get }
+    var weekEndMovieCellDatas: Observable<[MovieCellData]> { get }
 }
 
 protocol HomeViewModel: HomeViewModelInput, HomeViewModelOutput {}
 
 final class DefaultHomeViewModel: HomeViewModel {
-    private(set) var date: String?
-    private(set) var itemPerPage: String?
-    private(set) var weekOption: WeekOption?
+    private let movieAPIUseCase = MovieAPIUseCase()
+    var dailyMovieCellDatas = Observable<[MovieCellData]>([])
+    var allWeekMovieCellDatas = Observable<[MovieCellData]>([])
+    var weekEndMovieCellDatas = Observable<[MovieCellData]>([])
     
-    var movieCellDatas = Observable<[MovieCellData]>([])
-    
-    func setupProperty(date: String, itemPerPage: String = "10", weekOption: WeekOption? = nil) {
-        self.date = date
-        self.itemPerPage = itemPerPage
-        self.weekOption = weekOption
+    func requestDailyData(with date: String) {
+        movieAPIUseCase.requestDailyData(with: date, in: dailyMovieCellDatas)
     }
-    
-    func requestData() {
-        movieCellDatas.value = []
-        fetchDailyBoxOfficeData { boxOfficeList in
-            boxOfficeList.forEach { boxOffice in
-                self.fetchMovieDetailInfo(with: boxOffice.movieCd) { movieName, openDate, productYear in
-                    let openYear = String(openDate.prefix(4))
-                    DispatchQueue.main.async {
-                        self.fetchMoviePoster(with: movieName, year: openYear) { result in
-                            switch result {
-                            case .success(let url):
-                                self.appendCellData(boxOffice: boxOffice, openDate: openDate, posterURL: url)
-                            case .failure(_):
-                                self.fetchMoviePoster(with: movieName, year: productYear) { result in
-                                    switch result {
-                                    case .success(let url):
-                                        self.appendCellData(boxOffice: boxOffice, openDate: openDate, posterURL: url)
-                                    case .failure(_):
-                                        self.appendCellData(boxOffice: boxOffice, openDate: openDate, posterURL: nil)
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+    func requestAllWeekData(with date: String) {
+        movieAPIUseCase.requestAllWeekData(with: date, in: allWeekMovieCellDatas)
     }
-
-    private func fetchDailyBoxOfficeData(_ completion: @escaping ([BoxOffice]) -> Void) {
-        let searchDailyBoxOfficeListAPI = SearchDailyBoxOfficeListAPI(
-            date: date!
-        )
-        searchDailyBoxOfficeListAPI.execute { result in
-            switch result {
-            case .success(let boxOfficeList):
-                completion(boxOfficeList.boxOfficeResult.dailyBoxOfficeList!)
-            case .failure(let error):
-                print(String(describing: error))
-            }
-        }
-    }
-    
-    private func fetchMovieDetailInfo(
-        with movieCode: String,
-        _ completion: @escaping (_ movieName: String, _ openDate: String, _ productYear: String) -> Void
-    ) {
-        let searchMovieInfoAPI = SearchMovieInfoAPI(movieCode: movieCode)
-        searchMovieInfoAPI.execute { result in
-            switch result {
-            case .success(let movieDetail):
-                let movieName = movieDetail.movieInfoResult.movieInfo.movieNmEn
-                let openDate = movieDetail.movieInfoResult.movieInfo.openDt
-                let productYear = movieDetail.movieInfoResult.movieInfo.prdtYear
-                completion(movieName, openDate, productYear)
-            case .failure(let error):
-                print(String(describing: error))
-            }
-        }
-    }
-    
-    private func fetchMoviePoster(with movieName: String,
-                                  year: String?,
-                                  _ completion: @escaping (Result<URL, Error>) -> Void) {
-        let searchMoviePosterAPI = SearchMoviePosterAPI(movieTitle: movieName, year: year)
-        searchMoviePosterAPI.execute { result in
-            switch result {
-            case .success(let posterInfo):
-                let url = URL(string: posterInfo.posterURLString())!
-                completion(.success(url))
-            case .failure(let error):
-                print(String(describing: error))
-                completion(.failure(error))
-            }
-        }
-    }
-    
-    private func appendCellData(boxOffice: BoxOffice,
-                                openDate: String,
-                                posterURL: URL?) {
-        movieCellDatas.value.append(
-            MovieCellData(
-                posterURL: posterURL ?? nil,
-                currentRank: boxOffice.rank,
-                totalAudience: boxOffice.audiAcc,
-                title: boxOffice.movieNm,
-                openDate: openDate,
-                isNewEntry: boxOffice.rankOldAndNew == "NEW",
-                rankChange: boxOffice.rankInten
-            )
-        )
+    func requestWeekEndData(with date: String) {
+        movieAPIUseCase.requestWeekEndData(with: date, in: weekEndMovieCellDatas)
     }
 }
