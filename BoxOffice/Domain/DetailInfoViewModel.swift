8cceff1b38d7aac41825977130ba560c63d796df
jcrescent61 <jcrescent61@gmail.com>
2023-01-05 18:31:32 +0900

    feat: DetailInfoViewModel 로직 구현

--- BoxOffice/Domain/DetailInfoViewModel.swift
+++ BoxOffice/Domain/DetailInfoViewModel.swift
@@ -6,11 +6,54 @@
 //
 
 import Foundation
+import Combine
 
-final class DetailInfoViewModel {
-    private let detailBoxOffice: CustomBoxOffice
+protocol DetailInfoViewModelInputInterface: AnyObject {
+    func onViewDidLoad()
+}
+
+protocol DetailInfoViewModelOutputInterface: AnyObject {
+    var customBoxOfficePublisher: PassthroughSubject<CustomBoxOffice, Never> { get }
+    var detailBoxOfficePublisher: PassthroughSubject<DetailBoxOffice, Never> { get }
+}
+
+protocol DetailInfoViewModelInterface: AnyObject {
+    var input: DetailInfoViewModelInputInterface { get }
+    var output: DetailInfoViewModelOutputInterface { get }
+}
+
+final class DetailInfoViewModel: DetailInfoViewModelInterface, DetailInfoViewModelOutputInterface {
     
-    init(detailBoxOffice: CustomBoxOffice) {
-        self.detailBoxOffice = detailBoxOffice
+    // MARK: DetailInfoViewModel
+    var input: DetailInfoViewModelInputInterface { self }
+    var output: DetailInfoViewModelOutputInterface { self }
+    
+    // MARK: DetailInfoViewModelOutputInterface
+    var customBoxOfficePublisher = PassthroughSubject<CustomBoxOffice, Never>()
+    var detailBoxOfficePublisher = PassthroughSubject<DetailBoxOffice, Never>()
+    
+    private var cancelable = Set<AnyCancellable>()
+    private let networkHandler = Networker()
+    private let customBoxOffice: CustomBoxOffice
+    private var detailBoxOffice: DetailBoxOffice?
+    
+    init(customBoxOffice: CustomBoxOffice) {
+        self.customBoxOffice = customBoxOffice
+        print(customBoxOffice)
+    }
+}
+
+extension DetailInfoViewModel: DetailInfoViewModelInputInterface {
+    func onViewDidLoad() {
+        customBoxOfficePublisher.send(customBoxOffice)
+        
+        networkHandler.request(BoxOfficeAPI.detailBoxOffice(movieCode: customBoxOffice.boxOffice.movieCode))
+            .sink { completion in
+                
+            } receiveValue: { [weak self] (model: DetailBoxOfficeConnection) in
+                guard let self = self else { return }
+                self.output.detailBoxOfficePublisher.send(model.result.movieInfo)
+            }
+            .store(in: &cancelable)
     }
 }
