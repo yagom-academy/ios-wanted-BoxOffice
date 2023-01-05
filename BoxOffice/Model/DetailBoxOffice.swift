48ebcc074014fbfa51091edbf830027c59b5a26d
jcrescent61 <jcrescent61@gmail.com>
2023-01-05 18:34:41 +0900

    refactor: 모델 수정

--- BoxOffice/Model/DetailBoxOffice.swift
+++ BoxOffice/Model/DetailBoxOffice.swift
@@ -7,26 +7,79 @@
 
 import Foundation
 
+struct DetailBoxOfficeConnection: Decodable {
+    let result: DetailBoxOfficeResult
+    
+    enum CodingKeys: String, CodingKey {
+        case result = "movieInfoResult"
+    }
+}
+
+struct DetailBoxOfficeResult: Decodable {
+    let movieInfo: DetailBoxOffice
+    
+    enum CodingKeys: String, CodingKey {
+        case movieInfo = "movieInfo"
+    }
+}
+
 struct DetailBoxOffice: Decodable {
     let movieCode: String
     let productionYear: String
     let openDate: String
     let showTime: String
-    let genreName: String
-    let dirctors: String
-    let actors: String
-    let watchGradeName: String
+    let genre: [BoxOfficeGenre]
+    let directors: [BoxOfficeDirectors]
+    let actors: [BoxOfficeActors]?
+    let audits: [BoxOfficeAudits]
+    let movieName: String
     let movieEnglishName: String
     
     enum CodingKeys: String, CodingKey {
-        case movieCode = "MovieCd"
+        case movieCode = "movieCd"
         case productionYear = "prdtYear"
         case openDate = "openDt"
         case showTime = "showTm"
-        case genreName = "genreNm"
-        case dirctors = "directors"
+        case genre = "genres"
+        case directors = "directors"
         case actors = "actors"
-        case watchGradeName = "watchGradeNm"
+        case audits = "audits"
+        case movieName = "movieNm"
         case movieEnglishName = "movieNmEn"
+    }
+}
+
+
+struct BoxOfficeGenre: Decodable {
+    let genreName: String
+    
+    enum CodingKeys: String, CodingKey {
+        case genreName = "genreNm"
+    }
+}
+
+struct BoxOfficeDirectors: Decodable {
+    let peopleName: String?
+    let peopleEnglishName: String
+    
+    enum CodingKeys: String, CodingKey {
+        case peopleName = "peopleNm"
+        case peopleEnglishName = "peopleNmEn"
+    }
+}
+
+struct BoxOfficeActors: Decodable {
+    let peopleName: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case peopleName = "peopleNm"
+    }
+}
+
+struct BoxOfficeAudits: Decodable {
+    let watchGrade: String
+    
+    enum CodingKeys: String, CodingKey {
+        case watchGrade = "watchGradeNm"
     }
 }
