32351487bad25a7f68e05c316c431eecccee7cac
Judy <joo00ee@naver.com>
2023-01-05 09:12:26 +0900

    feat: 두 번째 셀인 MovieSubInfoCollectionViewCell 생성 #10

---
+++ BoxOffice/Scene/MovieDetailScene/MovieSubInfoCollectionViewCell.swift
@@ -0,0 +1,72 @@
+//
+//  MovieSubInfoCollectionViewCell.swift
+//  BoxOffice
+//
+//  Created by Judy on 2023/01/05.
+//
+
+import UIKit
+
+class MovieSubInfoCollectionViewCell: UICollectionViewCell {
+    private let horizontalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private let entireStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private let totalAudienceLabel = MovieLabel(font: .title3)
+    private let productionYearLabel = MovieLabel(font: .headline)
+    private let showTimeLabel = MovieLabel(font: .headline)
+    private let directorNameLabel = MovieLabel(font: .body)
+    private let actorsLabel = MovieLabel(font: .body)
+    private let ageLimitLabel = MovieLabel(font: .title3)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    private func setupView() {
+        addSubView()
+        setupConstraint()
+    }
+    
+    private func addSubView() {
+        horizontalStackView.addArrangedSubview(productionYearLabel)
+        horizontalStackView.addArrangedSubview(ageLimitLabel)
+        horizontalStackView.addArrangedSubview(showTimeLabel)
+        horizontalStackView.addArrangedSubview(totalAudienceLabel)
+        
+        entireStackView.addArrangedSubview(horizontalStackView)
+        entireStackView.addArrangedSubview(directorNameLabel)
+        entireStackView.addArrangedSubview(actorsLabel)
+        
+        self.addSubview(entireStackView)
+    }
+    
+    private func setupConstraint() {
+        NSLayoutConstraint.activate([
+            entireStackView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor,
+                                                 constant: 8),
+            entireStackView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor,
+                                                 constant: -8),
+            entireStackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor,
+                                                 constant: 16),
+            entireStackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor,
+                                                 constant: -16)
+        ])
+    }
+}
