2dddddcf8fedbb3753690512b862724f40bd4ebe
yeeton37 <lyeeun37@naver.com>
2023-01-06 01:40:29 +0900

    feat: MovieDetailViewModel 구현

--- BoxOffice/Sources/Presentation/Detail/MovieDetailViewModel.swift
+++ BoxOffice/Sources/Presentation/Detail/MovieDetailViewModel.swift
@@ -6,9 +6,51 @@
 //
 
 import Foundation
+import Combine
+import FirebaseFirestore
+import Firebase
 
-protocol MovieDetailViewModel {}
+protocol MovieDetailViewModelInput {
+    func viewWillAppear()
+    func deleteReview()
+}
 
-final class DefaultMovieDetailViewModel: MovieDetailViewModel {
+protocol MovieDetailViewModelOutput {
+    var movieModelPublisher: PassthroughSubject<Movie, Never> { get }
+}
+
+protocol MovieDetailViewModelInterface {
+    var input: MovieDetailViewModelInput { get }
+    var output: MovieDetailViewModelOutput { get }
+}
+
+final class MovieDetailViewModel: MovieDetailViewModelInterface  {
+    let firebaseManager = FirebaseManager(collectionType: Review.self as! FirebaseModel)
+    var input: MovieDetailViewModelInput { self }
+    var output: MovieDetailViewModelOutput { self }
+    var movieModelPublisher = PassthroughSubject<Movie, Never>()
+    @Published var movie: Movie
+    var reviews: [Review]?
     
+    init(movie: Movie) {
+        self.movie = movie
+    }
+    
+    private var cancelable = Set<AnyCancellable>()
+}
+
+extension MovieDetailViewModel: MovieDetailViewModelInput, MovieDetailViewModelOutput {
+    func viewWillAppear() {
+        firebaseManager.fetchAll { [weak self] in
+            if let self = self {
+                self.reviews = $0.filter {
+                    $0.movieName == self.movie.name
+                }
+            }
+        }
+    }
+    
+    func deleteReview() {
+        
+    }
 }
