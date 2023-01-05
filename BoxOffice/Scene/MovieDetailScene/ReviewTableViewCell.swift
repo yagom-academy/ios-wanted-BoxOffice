b5b146fc613c25670b4debf590e3d8be5c5d8499
Judy <joo00ee@naver.com>
2023-01-05 17:14:33 +0900

    feat: ReviewTableViewCell 생성 #10

---
+++ BoxOffice/Scene/MovieDetailScene/ReviewTableViewCell.swift
@@ -0,0 +1,105 @@
+//
+//  ReviewTableViewCell.swift
+//  BoxOffice
+//
+//  Created by 김주영 on 2023/01/05.
+//
+
+import UIKit
+
+class ReviewTableViewCell: UITableViewCell {
+    private let photoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .systemGray6
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let deleteButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "trash"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let nickNameLabel = MovieLabel(font: .headline)
+    private let contentLabel = MovieLabel(font: .body)
+    private let ratingView = StarRatingView()
+    
+    private let titleStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        stackView.spacing = 8
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private let contentStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        stackView.spacing = 8
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private let infoStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private let entireStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+    }
+    
+    private func setupView() {
+        addSubView()
+        setupConstraint()
+    }
+    
+    private func addSubView() {
+        titleStackView.addArrangedSubview(nickNameLabel)
+        titleStackView.addArrangedSubview(ratingView)
+        
+        contentStackView.addArrangedSubview(contentLabel)
+        contentStackView.addArrangedSubview(deleteButton)
+        
+        infoStackView.addArrangedSubview(titleStackView)
+        infoStackView.addArrangedSubview(contentStackView)
+        
+        entireStackView.addArrangedSubview(photoImageView)
+        entireStackView.addArrangedSubview(infoStackView)
+        
+        self.addSubview(entireStackView)
+    }
+    
+    private func setupConstraint() {
+        NSLayoutConstraint.activate([
+            entireStackView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor,
+                                                 constant: 8),
+            entireStackView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor,
+                                                 constant: -8),
+            entireStackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor,
+                                                 constant: 16),
+            entireStackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor,
+                                                 constant: -16)
+        ])
+    }
+}
