776931054800e2ebbabcc0bb5b39eb184d2bd527
wongbingg <wb277@naver.com>
2023-01-06 13:59:53 +0900

    # feat: 도메인 모델 변경 # 13

---
+++ BoxOffice/Domain/Entities/MovieData.swift
@@ -0,0 +1,26 @@
+//
+//  MovieData.swift
+//  BoxOffice
+//
+//  Created by 이원빈 on 2023/01/06.
+//
+
+import UIKit
+
+struct MovieData: Hashable {
+    let uuid: UUID
+    let poster: UIImage?
+    let currentRank: String
+    let title: String
+    let openDate: String
+    let totalAudience: String
+    let rankChange: String
+    let isNewEntry: Bool
+    let productionYear: String
+    let openYear: String
+    let showTime: String
+    let genreName: String
+    let directorName: String
+    let actors: [String]
+    let ageLimit: String
+}
