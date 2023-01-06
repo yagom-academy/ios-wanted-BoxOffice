875bcff48b2626d0282a0249beea0444560ea003
Judy <joo00ee@naver.com>
2023-01-06 17:43:05 +0900

    feat: 별점 평균을 계산하는 메서드 추가 #17
    - MovieReviewViewModel - calculateRating

--- BoxOffice/Scene/WriteReviewScene/MovieReviewViewModel.swift
+++ BoxOffice/Scene/WriteReviewScene/MovieReviewViewModel.swift
@@ -12,6 +12,7 @@ protocol MovieReviewViewModelInput {
 
 protocol MovieReviewViewModelOutput {
     var reviews: Observable<[Review]> { get set }
+    var rating: Observable<String> { get set }
     var error: Observable<String?> { get set }
 }
 
@@ -22,6 +23,7 @@ final class MovieReviewViewModel: MovieReviewViewModelType {
     
     /// Output
     var reviews: Observable<[Review]> = Observable([])
+    var rating: Observable<String> = Observable("")
     var error: Observable<String?> = Observable(nil)
     
     /// Input
@@ -41,9 +43,19 @@ final class MovieReviewViewModel: MovieReviewViewModelType {
             switch result {
             case .success(let reviews):
                 self?.reviews.value = reviews
+                self?.calculateRating()
             case .failure(let error):
                 self?.error.value = error.localizedDescription
             }
         }
     }
+    
+    func calculateRating() {
+        let ratings = reviews.value.map { $0.rating }
+        let ratingValues = ratings.compactMap { Double($0) }
+        let ratingSum = ratingValues.reduce(0, +)
+        
+        rating.value = String(format: "%.1f", ratingSum / Double(ratingValues.count))
+    }
 }
+
