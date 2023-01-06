aafb123ada2fbb662904b1a1d30e81c9f87c688e
minsson <mskaang@gmail.com>
2023-01-06 17:43:46 +0900

    [Feat] #10 리뷰를 서버에 등록하는 기능 구현

--- BoxOffice/Presentation/MovieReview/MovieReviewViewModel.swift
+++ BoxOffice/Presentation/MovieReview/MovieReviewViewModel.swift
@@ -17,8 +17,8 @@ final class MovieReviewViewModel {
     
     // MARK: - Actions
     var presentImagePicker: (() -> Void)?
-    var retrieveSelectedPhoto: (() -> Void)?
-    var presentViewController: ((UIViewController) -> Void)?
+    var showAlert: ((UIAlertController) -> Void)?
+    var popViewController: (() -> Void)?
     
     func viewDidLoad() {
         
@@ -35,10 +35,26 @@ final class MovieReviewViewModel {
     func registrationButtonTapped(movieReview: MovieReview) {
         if isValid(movieReview: movieReview) {
             // TODO: upload review
+            guard let selectedPhoto = selectedPhoto else {
+                return
+            }
+            
+            uploadReviewUseCase.execute(image: selectedPhoto, review: movieReview) { [weak self] result in
+                switch result {
+                case .success:
+                    self?.popViewController?()
+                case .failure(let error):
+                    let alert = UIAlertController(title: "리뷰가 정상적으로 등록되지 않았어요", message: "네트워크 상태를 확인해주세요.", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "확인", style: .cancel))
+                    
+                    self?.showAlert?(alert)
+                    print(error)
+                }
+            }
         } else {
             let alert = UIAlertController(title: "빠진 정보가 있는 것 같아요.", message: "별점, 관람평, 닉네임, 패스워드를 모두 채워주셨나요?", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "확인", style: .cancel))
-            presentViewController?(alert)
+            showAlert?(alert)
         }
     }
     
