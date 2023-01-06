62da4fad6361c02c37ca3e30ca6e7837e6b76c75
BaekGom <jihunmm02@gmail.com>
2023-01-06 15:29:47 +0900

    feat: CustomError 구현

---
+++ BoxOffice/BoxOfficeDetail/CustomError.swift
@@ -0,0 +1,13 @@
+//
+//  CustomError.swift
+//  BoxOffice
+//
+//  Created by Baek on 2023/01/06.
+//
+
+import Foundation
+
+enum CustomError: Error {
+    case statusCodeError
+    case unkownError
+}
