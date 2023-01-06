b1085092440c9f160900f5fb219b0df33c91047d
Ari <lee_ari95@icloud.com>
2023-01-07 00:06:15 +0900

    fix: 리뷰 저장이 제대로 되지 않던 현상을 개선
    - 이미지 사이즈를 조절하여 저장할 수 있도록 수정

--- BoxOffice/Sources/Extensions/UI/UIImage+extension.swift
+++ BoxOffice/Sources/Extensions/UI/UIImage+extension.swift
@@ -10,7 +10,7 @@ import UIKit
 extension UIImage {
     var toString: String? {
         get {
-            return self.jpegData(compressionQuality: 1.0)?.base64EncodedString()
+            return self.compress().jpegData(compressionQuality: 1.0)?.base64EncodedString()
         }
     }
     
