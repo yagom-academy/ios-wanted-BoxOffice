2d0f775c8eb4c5c8d70f878ee967fa4a743bc06f
wongbingg <wb277@naver.com>
2023-01-06 22:11:04 +0900

    refactor: Date+Extension 추가
    - 이전날짜를 구할 수 있는 메서드 구현

--- BoxOffice/Service/Network/Extension/Date+Extension.swift
+++ BoxOffice/Service/Network/Extension/Date+Extension.swift
@@ -13,7 +13,25 @@ extension Date {
         dateFormatter.dateFormat = "yyyyMMdd"
         return dateFormatter
     }
+    var calendar: Calendar {
+        return Calendar.current
+    }
+    
     func toString() -> String {
         dateFormatter.string(from: self)
+    }
+    
+    func toDate(from dateText: String) -> Date {
+        guard let date = dateFormatter.date(from: dateText) else {
+            return Date()
+        }
+        return date
+    }
+    
+    func previousDate(to day: Int) -> Date {
+        guard let previousDate = calendar.date(byAdding: .day, value: day, to: Date()) else {
+            return Date()
+        }
+        return previousDate
     }
 }
