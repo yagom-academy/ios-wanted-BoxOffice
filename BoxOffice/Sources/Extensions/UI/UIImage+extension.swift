33b869277b6aca7a6417e610c49e08fee8bc80db
yeeton37 <lyeeun37@naver.com>
2023-01-06 19:05:16 +0900

    feat: 이미지 리사이징 기능 추가

--- BoxOffice/Sources/Extensions/UI/UIImage+extension.swift
+++ BoxOffice/Sources/Extensions/UI/UIImage+extension.swift
@@ -13,4 +13,26 @@ extension UIImage {
             return self.jpegData(compressionQuality: 1.0)?.base64EncodedString()
         }
     }
+    
+    private func resize(percentage: CGFloat) -> UIImage {
+        let size = CGSize(width: size.width * percentage, height: size.height * percentage)
+        return UIGraphicsImageRenderer(size: size, format: imageRendererFormat).image { _ in
+            draw(in: CGRect(origin: .zero, size: size))
+        }
+    }
+    
+    func compress() -> UIImage {
+        var compressedImage = self
+        var quality: CGFloat = 1
+        let maxDataSize = 307200
+        guard var compressedImageData = compressedImage.jpegData(compressionQuality: 1) else {
+            return UIImage()
+        }
+        while compressedImageData.count > maxDataSize {
+            quality *= 0.8
+            compressedImage = compressedImage.resize(percentage: quality)
+            compressedImageData = compressedImage.jpegData(compressionQuality: quality) ?? Data()
+        }
+        return compressedImage
+    }
 }
