dc6adc9fdc3f600190af4cdce709f222157eaa19
wongbingg <wb277@naver.com>
2023-01-06 20:21:35 +0900

    refactor: MovieMainInfoView 구성 #16

--- BoxOffice/Scene/MovieDetailScene/SubViews/MovieMainInfoView.swift
+++ BoxOffice/Scene/MovieDetailScene/SubViews/MovieMainInfoView.swift
@@ -18,6 +18,7 @@ class MovieMainInfoView: UIView {
     private let openYearStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
+        stackView.spacing = 20
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
@@ -32,6 +33,8 @@ class MovieMainInfoView: UIView {
     private let infoStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.spacing = 20
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
@@ -42,6 +45,15 @@ class MovieMainInfoView: UIView {
         stackView.spacing = 8
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
+    }()
+    
+    private let fakeView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.widthAnchor.constraint(equalToConstant: 10).isActive = true
+        view.heightAnchor.constraint(greaterThanOrEqualToConstant: 10).isActive = true
+        view.setContentHuggingPriority(.init(100), for: .vertical)
+        return view
     }()
     
     private let posterView: UIImageView = {
@@ -55,17 +67,42 @@ class MovieMainInfoView: UIView {
         let imageView = UIImageView()
         imageView.image = UIImage(systemName: "star.fill")
         imageView.tintColor = .systemYellow
+        imageView.contentMode = .scaleAspectFit
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
+    private let rankChangeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.preferredFont(forTextStyle: .footnote)
+        label.layer.backgroundColor = UIColor.systemGreen.cgColor
+        label.layer.cornerRadius = 5
+        return label
+    }()
+    
+    private let isNewEntryLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.preferredFont(forTextStyle: .footnote)
+        label.layer.backgroundColor = UIColor.systemYellow.cgColor
+        label.layer.cornerRadius = 5
+        return label
+    }()
+    
+    private let rankBackgroundView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .black
+        view.alpha = 0.5
+        return view
+    }()
+    
+    private let currentRanklabel = MovieLabel(font: .largeTitle, isBold: true)
     private let titleLabel = MovieLabel(font: .title1, isBold: true)
-    private let currentRanklabel = MovieLabel(font: .callout)
-    private let rankChangeLabel = MovieLabel(font: .callout)
-    private let isNewEntryLabel = MovieLabel(font: .callout)
     private let openYearLabel = MovieLabel(font: .body)
     private let genreLabel = MovieLabel(font: .body)
-    private let ratingLabel = MovieLabel(font: .headline)
+    private let ratingLabel = MovieLabel(font: .largeTitle)
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -90,26 +127,47 @@ class MovieMainInfoView: UIView {
     //TODO: 정보 표시 형식 맞추기
     func configure(with movie: MovieData) {
         //TODO: 포스터 이미지로 변경
-        posterView.image = UIImage(systemName: "camera")
+        posterView.image = movie.poster
         //TODO: 별점 평균내기
         ratingLabel.text = "4.5"
         titleLabel.text = movie.title
         currentRanklabel.text = movie.currentRank
-        rankChangeLabel.text = movie.rankChange
-        isNewEntryLabel.text = "\(movie.isNewEntry)"
+        setRankChangeLabel(with: movie.rankChange)
+        setIsNewEntryLabel(with: movie.isNewEntry)
         openYearLabel.text = movie.openYear
         genreLabel.text = movie.genreName
+    }
+    
+    private func setRankChangeLabel(with rankChange: String) {
+        if Int(rankChange) ?? 0 > 0 {
+            rankChangeLabel.text = "  " + rankChange + "▲" + "  "
+            rankChangeLabel.layer.backgroundColor = UIColor.systemGreen.cgColor
+        } else if Int(rankChange) ?? 0 < 0 {
+            rankChangeLabel.text = "  " + rankChange + "▼" + "  "
+            rankChangeLabel.layer.backgroundColor = UIColor.systemRed.cgColor
+        } else {
+            rankChangeLabel.isHidden = true
+        }
+    }
+    
+    private func setIsNewEntryLabel(with isNewEntry: Bool) {
+        if isNewEntry {
+            isNewEntryLabel.text = " 신규진입 "
+        } else {
+            isNewEntryLabel.text = ""
+        }
     }
     
     private func setupView() {
         addSubView()
         setupConstraint()
         titleLabel.numberOfLines = 0
+        titleLabel.setContentHuggingPriority(UILayoutPriority(900), for: .vertical)
+        currentRanklabel.textColor = .white
         self.backgroundColor = .systemBackground
     }
     
     private func addSubView() {
-        rankStackView.addArrangedSubview(currentRanklabel)
         rankStackView.addArrangedSubview(rankChangeLabel)
         rankStackView.addArrangedSubview(isNewEntryLabel)
         
@@ -123,24 +181,35 @@ class MovieMainInfoView: UIView {
         infoStackView.addArrangedSubview(rankStackView)
         infoStackView.addArrangedSubview(openYearStackView)
         infoStackView.addArrangedSubview(ratingStackView)
+        infoStackView.addArrangedSubview(fakeView)
         
         entireStackView.addArrangedSubview(posterView)
         entireStackView.addArrangedSubview(infoStackView)
         
         self.addSubview(entireStackView)
+        addSubview(rankBackgroundView)
+        rankBackgroundView.addSubview(currentRanklabel)
     }
     
     //TODO: constraint 및 StackView 조정
     private func setupConstraint() {
         NSLayoutConstraint.activate([
+            currentRanklabel.centerXAnchor.constraint(equalTo: rankBackgroundView.centerXAnchor),
+            currentRanklabel.centerYAnchor.constraint(equalTo: rankBackgroundView.centerYAnchor),
+            
+            rankBackgroundView.topAnchor.constraint(equalTo: posterView.topAnchor),
+            rankBackgroundView.leadingAnchor.constraint(equalTo: posterView.leadingAnchor),
+            rankBackgroundView.widthAnchor.constraint(equalTo: posterView.widthAnchor, multiplier: 0.2),
+            rankBackgroundView.heightAnchor.constraint(equalTo: rankBackgroundView.widthAnchor),
+            
             entireStackView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor,
                                                  constant: 8),
             entireStackView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor,
-                                                 constant: -8),
+                                                    constant: -8),
             entireStackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor,
-                                                 constant: 16),
+                                                     constant: 16),
             entireStackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor,
-                                                 constant: -16),
+                                                      constant: -16),
             
             posterView.widthAnchor.constraint(equalTo: safeAreaLayoutGuide.widthAnchor,
                                               multiplier: 4/10)
